c231f1e54b3dd8f9c3879da4246c47a1e5969098
Ilya Bazhenov <31971067+bazhil@users.noreply.github.com>
2018-02-14 20:04:14 +0300

    Add files via upload

--- soap.py
+++ soap.py
@@ -3,41 +3,41 @@ import osa
 import requests
 import re
 from datetime import datetime
-
-client = osa.Client('http://www.webservicex.net/ConvertTemperature.asmx?WSDL')
-
-def temp_converter():
-    file = open('temps.txt', 'r')
-    # print(file)
-    temps = []
-    for line in file:
-        # print(line)
-        result = re.match('\d+', line)
-        print(result.group(0))
-        temps.append(int(result.group(0)))
-    average_temp = sum(temps)/len(temps)
-    print(average_temp)
-
-    result = client.service.ConvertTemp(average_temp, 'degreeFahrenheit', 'degreeCelsius')
-    print(result)
-
-temp_converter()
-
-client = osa.Client('http://fx.currencysystem.com/webservices/CurrencyServer4.asmx?WSDL')
-
-def currency_converter():
-    file = open('currencies.txt', 'r')
-    cost = 0.0
-    for line in file:
-        result = re.findall('\S+', line)
-        date = datetime.now().date()
-        print(result)
-        resultat = client.service.ConvertToNum('', result[2], 'RUB', result[1], True, '', '')
-        # print(resultat)
-        cost += round(resultat)
-        print(cost)
-
-currency_converter()
+#
+# client = osa.Client('http://www.webservicex.net/ConvertTemperature.asmx?WSDL')
+#
+# def temp_converter():
+#     file = open('temps.txt', 'r')
+#     # print(file)
+#     temps = []
+#     for line in file:
+#         # print(line)
+#         result = re.match('\d+', line)
+#         print(result.group(0))
+#         temps.append(int(result.group(0)))
+#     average_temp = sum(temps)/len(temps)
+#     print(average_temp)
+#
+#     result = client.service.ConvertTemp(average_temp, 'degreeFahrenheit', 'degreeCelsius')
+#     print(result)
+#
+# temp_converter()
+#
+# client = osa.Client('http://fx.currencysystem.com/webservices/CurrencyServer4.asmx?WSDL')
+#
+# def currency_converter():
+#     file = open('currencies.txt', 'r')
+#     cost = 0.0
+#     for line in file:
+#         result = re.findall('\S+', line)
+#         date = datetime.now().date()
+#         print(result)
+#         resultat = client.service.ConvertToNum('', result[2], 'RUB', result[1], True, '', '')
+#         # print(resultat)
+#         cost += round(resultat)
+#         print(cost)
+#
+# currency_converter()
 
 client = osa.Client('http://www.webservicex.net/length.asmx?WSDL')
 
@@ -47,10 +47,11 @@ def miles_converter():
     kilometres_distance = 0.00
     distances = []
     for line in file:
-        # line = line.replace(',', '')
+        line = line.replace(',', '')
         # print(line)
-        result = re.findall('\d*\,\d*\.\d*', line)
-        miles_distance = str(result[0])
+        result = re.findall('\d*\.\d*', line)
+        # print(result)
+        miles_distance = int(result[0])
         kilometres_distance = client.service.ChangeLengthUnit(miles_distance, '', 'Miles', 'Kilometres')
         print(kilometres_distance)
 
